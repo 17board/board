83e054e9d5a7a644d872d9a11136ed0658c83521
wangwei1237 <wangwei1237@gamil.com>
2015-07-05 11:13:14 +0800

    在控制器基类中增加参数获取的函数，方便后续使用

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -9,6 +9,8 @@ class ControllerBase extends Controller
     {
         $this->result = array('errno'  => 0,
                               'errmsg' => 'successful');
+        // 暂时先不用，后续会迁移过来。
+        //$this->initRequestParams();
     }
 
     protected function forward($uri)
@@ -24,6 +26,51 @@ class ControllerBase extends Controller
     	);
     }
 
+    protected function initRequestParams() {
+        $uri        = $this->request->getURI();
+        $routerInfo = $this->getRouterInfo($uri);
+
+        $uriKey     = $routerInfo['controller'] . '_' . $routerInfo['action'] . '_keys';
+        $this->requestParams = array();
+        foreach ($this->uriParams[$uriKey] as $key=>$castFunction) {
+            $$key = $this->request->getPost($key);
+            if (empty($$key)) {
+                $$key = $this->request->get($key);
+            }  
+            $this->requestParams[$key] = $$key;
+        }
+    }
+
+    /**
+     * @breif 获取请求中的请求信息获取请求的phalcon默认路由信息。
+     *        uri: /board/feature/new?projectid=13&content=%E6%88%91%E4%BB%AC
+     * @params $uri 请求的url地址。
+     * @return {module, controller, action}
+     */
+    protected function getRouterInfo($uri) {
+        $uri    = trim($uri, '/');
+        $arrURI = explode('?', $uri);
+        $path   = $arrURI[0];
+        $routerInfo = explode('/', $path);
+        
+        return array('module'     => $routerInfo[0],
+                     'controller' => $routerInfo[1],
+                     'action'     => $routerInfo[2]);
+    }
+
     protected $requestParams = array();
     protected $result        = array();
+
+    // 用户注册接口需要的字段.后续会放到配置里。
+    private $uriParams  = array(
+                                'feature_new_keys' => 
+                                        array('projectid' => 'intval',
+                                               'content'   => 'strval',
+                                               'uid'       => 'intval'),
+                                'user_register_keys' => 
+                                        array('username'  => 'strval', 
+                                              'password'  => 'md5', 
+                                              'nickname'  => 'strval', 
+                                              'email'     => 'strval', 
+                                              'role'      => 'intval',),);
 }
